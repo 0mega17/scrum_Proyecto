4f2d8e100b603a3ff552c97382f98070f173d0ef
estiven-18 <nelsoncuartas21@gmail.com>
2025-11-19 09:53:49 -0500

    crear aprendices hecho

---
+++ CONTROLLER/crear_aprenidez_controller.php
@@ -0,0 +1,47 @@
+<?php
+if (session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
+require_once '../MODEL/model.php';
+$mysql = new MySQL();
+$mysql->conectar();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['accion'])) {
+
+    if ($_POST['accion'] == 'crear') {
+        $id = $_POST['id'];
+        $nombre = filter_var($_POST['nombre'], FILTER_SANITIZE_SPECIAL_CHARS);
+        $email = $_POST['email'];
+        $email = filter_var(trim($email), FILTER_SANITIZE_EMAIL);
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        $ficha=$_POST['ficha'];
+
+
+
+        $verificarEmail = "SELECT id FROM aprendices WHERE email = '$email'";
+        $resultadoVerificacion = $mysql->efectuarConsulta($verificarEmail);
+
+        if ($resultadoVerificacion && mysqli_num_rows($resultadoVerificacion) > 0) {
+            echo json_encode(["status" => "error", "message" => "El correo electrónico ya está registrado."]);
+            exit;
+        }
+
+
+        $verificarId = "SELECT id FROM aprendices WHERE id = '$id'";
+        $resultadoId = $mysql->efectuarConsulta($verificarId);
+
+        if ($resultadoId && mysqli_num_rows($resultadoId) > 0) {
+            echo json_encode(["status" => "error", "message" => "El ID ya está registrado."]);
+            exit;
+        }
+
+        $consulta = "INSERT INTO aprendices (id,nombre, email, password, estado,fichas_id) VALUES ('$id','$nombre', '$email', '$password','activo','$ficha')";
+        $resultado = $mysql->efectuarConsulta($consulta);
+        if ($resultado) {
+            echo json_encode(["status" => "success"]);
+        } else {
+            echo json_encode(["status" => "error", "message" => "Error al crear Instructor"]);
+        }
+    }
+}
+$mysql->desconectar();
