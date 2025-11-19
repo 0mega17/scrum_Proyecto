e60041230766d026d6b007e9284da0ac27e69d5f
0mega17 <167831619+0mega17@users.noreply.github.com>
2025-11-19 07:32:04 -0500

    Crear ficha completo

--- ASSETS/JS/alertaCrearFicha.js
+++ ASSETS/JS/alertaCrearFicha.js
@@ -1,22 +1,18 @@
-
 document
   .getElementById("formularioCrearFicha")
   .addEventListener("submit", function (algo) {
     algo.preventDefault();
- 
+
     let datos = new FormData(this);
 
     fetch("../CONTROLLER/controladorCrearFicha.php", {
       method: "POST",
       body: datos,
-
     })
-  
       .then(function (respuesta) {
         return respuesta.json();
       })
       .then(function (json) {
-     
         if (json.susses === true) {
           Swal.fire({
             title: "¡Ficha creada!",
@@ -24,15 +20,13 @@ document
             icon: "success",
             confirmButtonText: "Aceptar",
             confirmButtonColor: "#3085d6",
-          }).then(() => {
-            window.location.href = "login.php";
           });
 
           document.getElementById("formularioCrearFicha").reset();
         } else if (json.susses === false) {
           Swal.fire({
             title: "Error",
-            text: "Ocurrió un error al crear la ficha",
+            text: json.mensaje,
             icon: "error",
             confirmButtonText: "Reintentar",
             confirmButtonColor: "#d33",
