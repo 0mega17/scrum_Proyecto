55576fce1274b511829087621bf813d20cad1fe9
0mega17 <167831619+0mega17@users.noreply.github.com>
2025-11-21 14:58:26 -0500

    Avances en el apartado de calificaciones por parte del instructor

--- ASSETS/JS/cargar_calificaciones.js
+++ ASSETS/JS/cargar_calificaciones.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   fetch("../CONTROLLER/datos_calificaciones.php")
     .then((respuesta) => respuesta.json())
     .then((datos) => {
-    datosCalificacion.innerHTML = "";
+      datosCalificacion.innerHTML = "";
       datos.forEach((datos) => {
         let fila = document.createElement("tr");
         fila.innerHTML = `
@@ -11,13 +11,58 @@ document.addEventListener("DOMContentLoaded", () => {
               <td>${datos.nombre_trabajo}</td>
               <td>${datos.archivo}</td>
               <td>
-                    <textarea name="comentario" rows="3" style="width: 100%;"></textarea>
+                    <textarea class ="comentario" name="comentario" rows="3" style="width: 100%;"></textarea>
             </td>
             <td class="text-center">
-                <button type="button" class="btn btn-success btn-sm me-2 btnAprobrar">A</button>
-                <button type="button" class="btn btn-danger btn-sm btnDesaprovar">D</button>
+                <button type="button" class="btn btn-success btn-sm me-2 btnAprobar" data-id = "${datos.id}">A</button>
+                <button type="button" class="btn btn-danger btn-sm btnDesaprovar" data-id = "${datos.id}">D</button>
             </td>         `;
-          datosCalificacion.appendChild(fila);
+        datosCalificacion.appendChild(fila);
+      });
+
+      function enviarCalificacion(calificacion, boton) {
+        let comentario = boton.closest("tr").querySelector(".comentario").value;
+        let id = parseInt(boton.dataset.id);
+        fetch("../CONTROLLER/controlador_calificacion.php", {
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: `calificacion=${calificacion}&comentario=${comentario}&id=${id} `,
+        })
+          .then((respuesta) => respuesta.json())
+          .then((datos) => {
+            // aqui debe de salir la alerta  para cer que se
+            if (datos.validacion) {
+              Swal.fire({
+                icon: "success",
+                title: "¡Calificación realizada!",
+                text: datos.mensaje,
+                timer: 2000,
+                timerProgressBar: true,
+                showConfirmButton: false,
+              });
+            } else {
+              Swal.fire({
+                icon: "error",
+                title: "Ocurrió un error",
+                text: datos.mensaje,
+                timer: 2500,
+                timerProgressBar: true,
+                showConfirmButton: false,
+              });
+            }
+          });
+      }
+      document.querySelectorAll(".btnAprobar").forEach((boton) => {
+        boton.addEventListener("click", function () {
+          enviarCalificacion("Aprobado", this);
+        });
+      });
+
+      document.querySelectorAll(".btnDesaprovar").forEach((boton) => {
+        boton.addEventListener("click", function () {
+          enviarCalificacion("Desaprobado", this);
+        });
       });
     });
+  
 });
