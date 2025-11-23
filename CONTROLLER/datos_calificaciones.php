8043a73791c25dfc6e0508b6de1023c98d9dd331
0mega17 <167831619+0mega17@users.noreply.github.com>
2025-11-23 15:56:25 -0500

    Cambios logicos ahora solo el intructor puede ver los trabajos a entregar de solo las fichas a las que pertenece

--- CONTROLLER/datos_calificaciones.php
+++ CONTROLLER/datos_calificaciones.php
@@ -1,25 +1,24 @@
 <?php
 if (session_status() !== PHP_SESSION_ACTIVE) {
     session_start();
+    $idUsuario = $_SESSION['IDusuario'];
 }
 
- 
-        require_once '../MODEL/model.php';
-        header('Content-Type: application/json');
-        $mysql = new MySQL();
-        $mysql->conectar();
 
-        $consaultaDatosEntrega = "select entregas.id ,aprendices.nombre as nombre_aprendiz, trabajos.nombre as nombre_trabajo, entregas.archivo from entregas
+require_once '../MODEL/model.php';
+header('Content-Type: application/json');
+$mysql = new MySQL();
+$mysql->conectar();
+
+$consaultaDatosEntrega = "select entregas.id ,aprendices.nombre as nombre_aprendiz, trabajos.nombre as nombre_trabajo, entregas.archivo from entregas
 inner join aprendices on aprendices.id = entregas.aprendices_id inner join trabajos on trabajos.id = entregas.trabajos_id 
-where entregas.estado = 'Entregado'
+where entregas.estado = 'Entregado' and trabajos.instructores_id = $idUsuario
 ";
-        $resultado = $mysql->efectuarConsulta($consaultaDatosEntrega);
-        $entregas = [];
+$resultado = $mysql->efectuarConsulta($consaultaDatosEntrega);
+$entregas = [];
 
 
-        while ($fila = $resultado->fetch_assoc()) {
-            $entregas[] = $fila;
-        }
-        echo json_encode($entregas);
-
-
+while ($fila = $resultado->fetch_assoc()) {
+    $entregas[] = $fila;
+}
+echo json_encode($entregas);
